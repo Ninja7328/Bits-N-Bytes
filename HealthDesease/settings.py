ef8e2ddc4259cf2def2dc81b807cd9a666d45083
Ninja7328 <jadon7328@gmail.com>
2023-02-25 03:14:31 +0530

    Bits N' Bytes

--- HealthDesease/settings.py
+++ HealthDesease/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5_s&43)=k=zn)ch4gx(%zwbbj6zb$!ijbi8wk6em#spkk!yq$5'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -119,7 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+import os
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join ( BASE_DIR, 'staticFiles_build', 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = Path.joinpath(BASE_DIR,'media')
--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn HealthDesease.wsgi 
