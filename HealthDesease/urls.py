47529f8bef19980af39c42160ffc6e9ad827aa10
Ninja7328 <jadon7328@gmail.com>
2023-02-25 03:32:13 +0530

    Bits N' Bytes

--- HealthDesease/urls.py
+++ HealthDesease/urls.py
@@ -66,3 +66,5 @@ urlpatterns = [
     path('change_status/<int:pid>/', change_status, name="change_status")
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
